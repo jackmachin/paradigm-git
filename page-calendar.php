1b7b13ea9c5a6c70c840c6e5b31517b1f67ab2a2
jackmachin <jack.machin125@gmail.com>
2015-03-30 15:10:14 +0100

    Clean it up

--- page-calendar.php
+++ page-calendar.php
@@ -6,40 +6,40 @@ Template Name: Calendar Template
 
 <?php get_header(); ?>
 
-			<div id="content">
+    <div id="content">
 
-				<div id="inner-content" class="wrap clearfix">
+        <div id="inner-content" class="wrap clearfix">
 
-					<?php get_sidebar( 'members' ); ?>
+            <?php get_sidebar( 'members' ); ?>
 
-						<div id="main" class="eightcol clearfix" role="main">
+            <div id="main" class="eightcol clearfix" role="main">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+                    <article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-								<header class="article-header">
+                        <header class="article-header">
 
-								</header> <!-- end article header -->
+                        </header> <!-- end article header -->
 
-								<section class="entry-content clearfix" itemprop="articleBody">
-									<?php the_content(); ?>
-								</section> <!-- end article section -->
+                        <section class="entry-content clearfix" itemprop="articleBody">
+                            <?php the_content(); ?>
+                        </section> <!-- end article section -->
 
-								<footer class="article-footer">
+                        <footer class="article-footer">
 
-								</footer> <!-- end article footer -->
+                        </footer> <!-- end article footer -->
 
-							</article> <!-- end article -->
+                    </article> <!-- end article -->
 
-							<?php endwhile; else : ?>
+                <?php endwhile; else : ?>
 
-							<?php endif; ?>
+                <?php endif; ?>
 
-						</div> <!-- end #main -->
+            </div> <!-- end #main -->
 
-				</div> <!-- end #inner-content -->
+        </div> <!-- end #inner-content -->
 
-			</div> <!-- end #content -->
+    </div> <!-- end #content -->
 
 <?php get_footer(); ?>
