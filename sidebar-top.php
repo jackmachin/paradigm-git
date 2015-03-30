277858d725fbb465dee3a1204be9ec28dd2b10fe
jackmachin <jack.machin125@gmail.com>
2015-03-30 14:59:22 +0100

    Sidebar top

--- sidebar-top.php
+++ sidebar-top.php
@@ -1,9 +1,9 @@
-				<div id="sidebar-top" class="twelvecol first clearfix" role="complementary">
+<div id="sidebar-top" class="twelvecol first clearfix" role="complementary">
 
-					<?php if ( is_active_sidebar( 'sidebar-top' ) ) : ?>
+    <?php if ( is_active_sidebar( 'sidebar-top' ) ) : ?>
 
-						<?php dynamic_sidebar( 'sidebar-top' ); ?>
+        <?php dynamic_sidebar( 'sidebar-top' ); ?>
 
-					<?php endif; ?>
+    <?php endif; ?>
 
-				</div>
+</div>
