596e61ab2c0204c321d3eb95cc6c4efa7d08c3b0
jackmachin <jack.machin125@gmail.com>
2015-03-30 11:16:33 +0100

    Formattingf

--- page-partner.php
+++ page-partner.php
@@ -12,47 +12,47 @@ Template Name: Strategic Partner Template
 
             <?php get_sidebar( 'members' ); ?>
 
-                <div id="main" class="ninecol clearfix" role="main">
+            <div id="main" class="ninecol clearfix" role="main">
 
-                    <?php if (have_posts()) : while (have_posts()) : the_post();  ?>
+                <?php if (have_posts()) : while (have_posts()) : the_post();  ?>
 
-                        <article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+                    <article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-                            <header class="article-header">
+                        <header class="article-header">
 
-                                <?php if ( function_exists('yoast_breadcrumb') ) {
-                                    yoast_breadcrumb('<p id="breadcrumbs">','</p>');
-                                } ?>
+                            <?php if ( function_exists('yoast_breadcrumb') ) {
+                                yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+                            } ?>
 
-                                <h1><?php the_title ();?></h1>
-                            </header> <!-- end article header -->
+                            <h1><?php the_title ();?></h1>
+                        </header> <!-- end article header -->
 
-                            <section class="entry-content clearfix" itemprop="articleBody">
-                                <img src="<?php the_field ('logo');?>" class="alignright clearfix">
+                        <section class="entry-content clearfix" itemprop="articleBody">
+                            <img src="<?php the_field ('logo');?>" class="alignright clearfix">
 
-                                <?php the_field ('intro_text');?>
+                            <?php the_field ('intro_text');?>
 
-                                <?php if( have_rows('main_content') ): ?>
+                            <?php if( have_rows('main_content') ): ?>
 
-                                    <?php while( have_rows('main_content') ): the_row();?>
+                                <?php while( have_rows('main_content') ): the_row();?>
 
                                     <h2><a class="aj-collapse" rel="<?php the_sub_field ('section_id') ;?>"><?php  the_sub_field ('section_title') ; ?></a></h2>
 
                                     <div class="aj-hidden clearfix" id="<?php  the_sub_field ('section_id') ;?>"><?php  the_sub_field ('section_content') ; ?></div>
 
-                                    <?php endwhile; ?>
+                                <?php endwhile; ?>
 
-                                <?php endif; ?>
+                            <?php endif; ?>
 
-                                <?php the_field ('afterthoughts');?>
+                            <?php the_field ('afterthoughts');?>
 
-                            </section> <!-- end article section -->
+                        </section> <!-- end article section -->
 
-                        </article> <!-- end article -->
+                    </article> <!-- end article -->
 
-                        <?php endwhile; else : ?>
+                <?php endwhile; else : ?>
 
-                        <?php endif; ?>
+                <?php endif; ?>
 
             </div> <!-- end #main -->
 
