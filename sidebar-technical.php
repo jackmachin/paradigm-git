e614e0af9543ef7b95d636ae34afe6d99fc93aa8
jackmachin <jack.machin125@gmail.com>
2015-03-30 11:29:41 +0100

    Removing weird code

--- sidebar-technical.php
+++ sidebar-technical.php
@@ -1,20 +1,16 @@
-			<?php if ( current_user_can( 'read' ) ) { ?>
-
-				<div id="sidebar-technical" class="sidebar threecol first clearfix" role="complementary">
-					<?php if ( current_user_can( 'see_menu' ) ) { ?>
-						<nav role="navigation">
-							<?php bones_technical_nav(); ?>
-						</nav>
+<div id="sidebar-technical" class="sidebar threecol first clearfix" role="complementary">
+    <?php if ( current_user_can( 'see_menu' ) ) { ?>
+        <nav role="navigation">
+            <?php bones_technical_nav(); ?>
+        </nav>
 
 
-					<?php if ( is_active_sidebar( 'sidebar-technical' ) ) : ?>
+    <?php if ( is_active_sidebar( 'sidebar-technical' ) ) : ?>
 
-						<?php dynamic_sidebar( 'sidebar-technical' ); ?>
+        <?php dynamic_sidebar( 'sidebar-technical' ); ?>
 
-					<?php endif; ?>
+    <?php endif; ?>
 
-					<?php }  else dynamic_sidebar ('sidebar-members');?>
+    <?php }  else dynamic_sidebar ('sidebar-members');?>
 
-				</div>
-
-			<?php } ?>
+</div>
