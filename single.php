b0b0f08742eb07284c096dcaf69a7b3e9b906b41
jackmachin <jack.machin125@gmail.com>
2015-04-07 17:01:02 +0100

    Stuff moving

--- single.php
+++ single.php
@@ -163,24 +163,24 @@
 
             <?php else : ?>
 
-							<article id="post-not-found" class="hentry clearfix">
-									<header class="article-header">
-										<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-									</header>
-									<section class="entry-content">
-										<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-									</section>
-									<footer class="article-footer">
-											<p><?php _e( 'This is the error message in the single.php template.', 'bonestheme' ); ?></p>
-									</footer>
-							</article>
+                <article id="post-not-found" class="hentry clearfix">
+                        <header class="article-header">
+                            <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+                        </header>
+                        <section class="entry-content">
+                            <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+                        </section>
+                        <footer class="article-footer">
+                                <p><?php _e( 'This is the error message in the single.php template.', 'bonestheme' ); ?></p>
+                        </footer>
+                </article>
 
-						<?php endif; ?>
+            <?php endif; ?>
 
-					</div> <!-- end #main -->
+            </div> <!-- end #main -->
 
-				</div> <!-- end #inner-content -->
+        </div> <!-- end #inner-content -->
 
-			</div> <!-- end #content -->
+    </div> <!-- end #content -->
 
 <?php get_footer(); ?>
