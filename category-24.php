944a1bb1d7be5e87a3abd7d01c766d32b191c254
jackmachin <jack.machin125@gmail.com>
2015-04-08 11:26:34 +0100

    sfj

--- category-24.php
+++ category-24.php
@@ -4,37 +4,44 @@
 
 				<div id="inner-content" class="wrap clearfix">
 
-						<?php get_sidebar('technical'); ?>
+                    <?php get_sidebar('technical'); ?>
 
-						<div id="main" class="ninecol clearfix" role="main">
+                    <div id="main" class="ninecol clearfix" role="main">
 
-                    <div class="breadcrumbs">
+                        <div class="breadcrumbs">
                       <span xmlns:v="http://rdf.data-vocabulary.org/#">
-                        <span typeof="v:Breadcrumb"><a href="http://paradigmgroup.eu" rel="v:url" property="v:title">Home</a></span> » <span typeof="v:Breadcrumb"><a href="http://paradigmgroup.eu/members-area/" rel="v:url" property="v:title">Member's Area</a></span> » <span typeof="v:Breadcrumb"><a href="http://paradigmgroup.eu/members-area/safeguard/" rel="v:url" property="v:title">Safeguard</a></span> » <span typeof="v:Breadcrumb"><a href="http://paradigmgroup.eu/members-area/safeguard/technical-services/" rel="v:url" property="v:title">Technical Services</a></span> » <?php single_cat_title(); ?>
-                      </span>
-                    </div>
+                            <span typeof="v:Breadcrumb"><a href="http://paradigmgroup.eu" rel="v:url" property="v:title">Home</a></span> » <span typeof="v:Breadcrumb"><a href="http://paradigmgroup.eu/members-area/" rel="v:url" property="v:title">Member's Area</a></span> » <span typeof="v:Breadcrumb"><a href="http://paradigmgroup.eu/members-area/safeguard/" rel="v:url" property="v:title">Safeguard</a></span> » <span typeof="v:Breadcrumb"><a href="http://paradigmgroup.eu/members-area/safeguard/technical-services/" rel="v:url" property="v:title">Technical Services</a></span> » <?php single_cat_title(); ?>
+                          </span>
+                        </div>
 
-							<?php if (is_category()) { ?>
-								<h1 class="archive-title">
-									<?php single_cat_title(); ?>
-								</h1>
+                        <h1 class="archive-title"><?php single_cat_title(); ?></h1>
 
-							<?php } ?>
-
-														<div class="fourcol clearfix first"><h2><a href="/category/regulatory-updates/file-review-updates/">File Review Updates</a></h2></div>
-														<div class="fourcol clearfix"><h2><a href="/category/regulatory-updates/compliance-updates/">Compliance Updates</a></h2></div>
-														<div class="fourcol clearfix last"><h2><a href="/category/regulatory-updates/technical-updates/">Technical Updates</a></h2></div>
+                            <div class="fourcol clearfix first">
+                                <h2><a href="/category/regulatory-updates/file-review-updates/">File Review Updates</a></h2>
+                            </div>
+                            <div class="fourcol clearfix">
+                                <h2><a href="/category/regulatory-updates/compliance-updates/">Compliance Updates</a></h2>
+                            </div>
+                            <div class="fourcol clearfix last">
+                                <h2><a href="/category/regulatory-updates/technical-updates/">Technical Updates</a></h2>
+                            </div>
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-                    <article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
+                                <article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
 
-                        <header class="article-header">
+                                    <header class="article-header">
 
-                            <h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a> <span class="byline vcard"><?php
-              printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time>', 'bonestheme' ), get_the_time( 'Y-m-j' ), get_the_time( __( 'F jS, Y', 'bonestheme' ) ));
-              ?></span></h3>
-                        </header> <!-- end article header -->
+                                        <h3 class="h2">
+                                            <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+                                                <?php the_title(); ?>
+                                            </a>
+                                            <span class="byline vcard">
+                                                <?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time>', 'bonestheme' ), get_the_time( 'Y-m-j' ), get_the_time( __( 'F jS, Y', 'bonestheme' ) )); ?>
+                                            </span>
+                                        </h3>
+
+                                    </header> <!-- end article header -->
 
                         <div class="entry-content clearfix">
 
