2eb78f0d5c6a0e539c93f51cb97d8fafea2c2034
jackmachin <jack.machin125@gmail.com>
2015-09-29 14:20:13 +0100

    Strange

--- library/admin.php
+++ library/admin.php
@@ -196,9 +196,9 @@ add_action( 'manage_post_posts_custom_column', 'bs_event_table_content', 10, 2 )
 
 function bs_event_table_content( $column_name, $post_id ) {
     if ($column_name == 'members-cp') {
-    $status = get_post_meta( $post_id, '_members_access_role', true );
-        foreach($status as $status) {
-	       echo $status;
+    $permission = get_post_meta( $post_id, '_members_access_role', true );
+        foreach($permission as $permission) {
+	       echo $permission;
         }
     }
 }
