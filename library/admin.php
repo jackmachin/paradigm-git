ed28927e11633600cb8697b5a1562f0f968d3d38
jackmachin <jack.machin125@gmail.com>
2015-09-29 14:09:22 +0100

    Does this work?

--- library/admin.php
+++ library/admin.php
@@ -192,4 +192,13 @@ function bs_event_table_head( $defaults ) {
     return $defaults;
 }
 
+add_action( 'manage_event_posts_custom_column', 'bs_event_table_content', 10, 2 );
+
+function bs_event_table_content( $column_name, $post_id ) {
+    if ($column_name == 'members-cp') {
+    $status = get_post_meta( $post_id, '_members_access_role', true );
+    echo $status;
+    }
+}
+
 ?>
