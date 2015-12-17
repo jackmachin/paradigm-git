ab10680ea5addb3b6139e5f0e22bfa0a1374ca24
Jack Machin <jack.machin125@gmail.com>
2015-12-17 14:21:58 +0000

    Problems

--- category-target.php
+++ category-target.php
@@ -7,7 +7,7 @@
 					<div class="intro twelvecol">
 						<div class="description eightcol">
 
-                            <?php if (is_tax( 'issue', '368' )) { ?>
+                            <?php if (is_tax( 'issue', '100-dec' )) { ?>
    <h1>Welcome to the 100<sup>th</sup> Edition of Target</h1>
 
 <?php } else { ?>
