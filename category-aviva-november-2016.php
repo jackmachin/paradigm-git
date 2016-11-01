3703b45fe9d4ae6f21238d7fd7b24ab347884c6a
Jack Machin <jack.machin125@gmail.com>
2016-11-01 14:54:48 +0000

    Let's try this

--- category-aviva-november-2016.php
+++ category-aviva-november-2016.php
@@ -5,7 +5,11 @@
         <div id="inner-content" class="wrap clearfix">
 
             <div class="intro twelvecol first">
-                    <div id="description" ><?php echo category_description( $category_id ); ?> </div>
+
+                <div id="description" >
+                    <?php echo category_description( $category_id ); ?>
+                </div>
+
                 <div id="main" class="twelvecol clearfix first" role="main">
 
                     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
@@ -13,9 +17,7 @@
                     <article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
 
                         <header class="article-header">
-                            <?php if (has_post_format ('image') ){} else { ?>
-                                <h1 class="h2"><?php the_title(); ?></h1>
-                            <?php } ?>
+                            <h1><?php the_title(); ?></h1>
                         </header> <!-- end article header -->
 
                         <section class="entry-content clearfix">
@@ -32,30 +34,19 @@
 
                     <?php endwhile; ?>
 
-                            <?php if ( function_exists( 'bones_page_navi' ) ) { ?>
-                                    <?php bones_page_navi(); ?>
-                            <?php } else { ?>
-                                    <nav class="wp-prev-next">
-                                            <ul class="clearfix">
-                                                <li class="prev-link"><?php next_posts_link( __( '&laquo; Older Entries', 'bonestheme' )) ?></li>
-                                                <li class="next-link"><?php previous_posts_link( __( 'Newer Entries &raquo;', 'bonestheme' )) ?></li>
-                                            </ul>
-                                    </nav>
-                            <?php } ?>
-
                     <?php else : ?>
 
-                            <article id="post-not-found" class="hentry clearfix">
-                                    <header class="article-header">
-                                        <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-                                </header>
-                                    <section class="entry-content">
-                                        <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-                                </section>
-                                <footer class="article-footer">
-                                        <p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
-                                </footer>
-                            </article>
+                        <article id="post-not-found" class="hentry clearfix">
+                                <header class="article-header">
+                                    <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+                            </header>
+                                <section class="entry-content">
+                                    <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+                            </section>
+                            <footer class="article-footer">
+                                    <p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+                            </footer>
+                        </article>
 
                     <?php endif; ?>
 
